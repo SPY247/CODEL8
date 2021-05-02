431dedf9e868f63da6d4a38153b558cad8e8cb84
SPY247 <83517499+SPY247@users.noreply.github.com>
2021-05-02 23:12:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,69 +1,45 @@
-let c = document.getElementById("myCanvas");
-let ctx = c.getContext("2d");
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 
 let loadImage = (src, callback) => {
-  var img = document.createElement("img");
+  let img = document.createElement("img");
   img.onload = () => callback(img);
   img.src = src;
 };
-
-let setImagePath = (frameNumber, animation) => {
+let imagePath = (frameNumber, animation) => {
   return "images/" + animation + "/" + frameNumber + ".png";
 };
-
-let frame = {
+let frames = {
   idle: [1, 2, 3, 4, 5, 6, 7, 8],
   kick: [1, 2, 3, 4, 5, 6, 7],
   punch: [1, 2, 3, 4, 5, 6, 7],
-  block: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  forward: [1, 2, 3, 4, 5, 6],
-  backward: [1, 2, 3, 4, 5, 6],
 };
 
 let loadImages = (callback) => {
-  let images = {
-    idle: [],
-    kick: [],
-    punch: [],
-    block: [],
-    forward: [],
-    backward: [],
-  };
+  let images = { idle: [], kick: [], punch: [] };
   let imagesToLoad = 0;
 
-  ["idle", "kick", "punch", "block", "forward", "backward"].forEach(
-    (animation) => {
-      let animationFrames = frame[animation];
-      imagesToLoad = imagesToLoad + animationFrames.length;
+  ["idle", "kick", "punch"].forEach((animation) => {
+    let animationFrames = frames[animation];
+    imagesToLoad = imagesToLoad + animationFrames.length;
+    animationFrames.forEach((frameNumber) => {
+      let path = imagePath(frameNumber, animation);
 
-      animationFrames.forEach((frameNumber) => {
-        let path = setImagePath(frameNumber, animation);
-        loadImage(path, (image) => {
-          images[animation][frameNumber - 1] = image;
-          imagesToLoad = imagesToLoad - 1;
-
-          if (imagesToLoad === 0) callback(images);
-        });
+      loadImage(path, (image) => {
+        images[animation][frameNumber - 1] = image;
+        imagesToLoad = imagesToLoad - 1;
+        if (imagesToLoad === 0) {
+          callback(images);
+        }
       });
-    }
-  );
+    });
+  });
 };
-
-let x = 0;
 let animate = (ctx, images, animation, callback) => {
   images[animation].forEach((image, index) => {
     setTimeout(() => {
-      if (image.src.includes("forward") && x < 576) {
-        x += 20;
-        ctx.clearRect(x, 400, 100, 500);
-        ctx.drawImage(image, x, 100, 350, 350);
-      } else if (image.src.includes("backward") && x > 0) {
-        x -= 20;
-        ctx.clearRect(x, 100, 500, 500);
-        ctx.drawImage(image, x, 100, 350, 350);
-      }
-      ctx.clearRect(x, 0, 500, 500);
-      ctx.drawImage(image, x, 100, 350, 350);
+      ctx.clearRect(0, 0, 500, 500);
+      ctx.drawImage(image, 0, 0, 500, 500);
     }, index * 100);
   });
   setTimeout(callback, images[animation].length * 100);
@@ -71,55 +47,34 @@ let animate = (ctx, images, animation, callback) => {
 
 loadImages((images) => {
   let queuedAnimations = [];
+
   let aux = () => {
     let selectedAnimation;
 
-    if (queuedAnimations.length === 0) selectedAnimation = "idle";
-    else selectedAnimation = queuedAnimations.shift();
-
+    if (queuedAnimations.length === 0) {
+      selectedAnimation = "idle";
+    } else {
+      selectedAnimation = queuedAnimations.shift();
+    }
     animate(ctx, images, selectedAnimation, aux);
   };
+
   aux();
+  document.getElementById("kick").onclick = () => {
+    queuedAnimations.push("kick");
+  };
 
   document.getElementById("punch").onclick = () => {
     queuedAnimations.push("punch");
-    playSounds("punch");
-  };
-  document.getElementById("kick").onclick = () => {
-    queuedAnimations.push("kick");
-    playSounds("kick");
-  };
-  document.getElementById("block").onclick = () => {
-    queuedAnimations.push("block");
-    playSounds("block");
-  };
-  document.getElementById("forward").onclick = () => {
-    queuedAnimations.push("forward");
-    playSounds("move");
-  };
-  document.getElementById("backward").onclick = () => {
-    queuedAnimations.push("backward");
-    playSounds("move");
   };
 
   document.addEventListener("keyup", (event) => {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    const key = event.key;
 
     if (key === "ArrowLeft") {
       queuedAnimations.push("kick");
-      playSounds("kick");
     } else if (key === "ArrowRight") {
       queuedAnimations.push("punch");
-      playSounds("punch");
-    } else if (key === "ArrowUp") {
-      queuedAnimations.push("forward");
-      playSounds("move");
-    } else if (key === "ArrowDown") {
-      queuedAnimations.push("backward");
-      playSounds("move");
-    } else if (key === " ") {
-      queuedAnimations.push("block");
-      playSounds("block");
     }
   });
 });
